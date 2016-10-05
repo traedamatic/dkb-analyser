4af8900658b9ccc3461ca28556d3cbb7e316ea8c
Nicolas Traeder <traeder@codebility.com>
2016-10-05 21:07:14 +0200

    The homescreen printing method prints the current balance now. Adds more color text function to the printer file

--- printer.go
+++ printer.go
@@ -4,28 +4,48 @@ import "fmt"
 import (
 	"./account"
 	"strings"
+	"strconv"
 )
 
 func makeYellow(text string) string {
+	return fmt.Sprintf("\033[33m%s\033[39m", text)
+}
+
+func makeRed(text string) string {
+	return fmt.Sprintf("\033[31m%s\033[39m", text)
+}
+
+func makeGreen(text string) string {
 	return fmt.Sprintf("\033[32m%s\033[39m", text)
 }
 
-func PrintHomeScreen(a *account.Account) {
 
+func PrintHomeScreen(a *account.Account) {
 
 	//clear it
 	fmt.Print("\033c")
 
 	//print headline
 	fmt.Printf(
-		"%s - Analyses your CSV DKB files - Konto: %s - CSV von %s bis %s\033[1E",
+		"%s - Analyses your CSV DKB files - Konto: %s - CSV from %s until %s\033[1E",
 		makeYellow("DKB Analyser"),
 		a.Title,
 		a.BeginDate.Format("02.01.2006"),
 		a.EndDate.Format("02.01.2006"))
 
 
-	fmt.Println(strings.Repeat("-", 200))
+	fmt.Println(strings.Repeat("-", 120))
 
+	var balanceString string = makeGreen(strconv.FormatFloat(a.Balance, 'f', 2, 64))
+	if a.Balance < 0 {
+		balanceString = makeRed(strconv.FormatFloat(a.Balance, 'f', 2, 64))
+	}
+
+	//print headline
+	fmt.Printf(
+		"Current balance %s \033[1E",
+		balanceString)
+
+	fmt.Println(strings.Repeat("-", 120))
 
 }
